010d9f113f7b512ee57cda8258d6e10baa95e2bd
rehan-sakiwala07 <rehan.sakiwala@roimaint.com>
2026-02-09 11:41:20 +0530

    Backend | Organization entities

---
+++ backend/src/main/java/com/capestone/hrms_backend/entity/organization/Employee.java
@@ -0,0 +1,60 @@
+package com.capestone.hrms_backend.entity.organization;
+
+import com.capestone.hrms_backend.entity.shared.Base;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+
+
+@Entity
+@Table(name = "employees")
+@Getter
+@Setter
+public class Employee extends Base {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column(length = 50, name = "first_name")
+    private String firstName;
+
+    @Column(length = 50, name = "middle_name")
+    private String middleName;
+
+    @Column(length = 50, name = "last_name")
+    private String lastName;
+
+    @Column(length = 50)
+    private String designation;
+
+
+    private LocalDate dob;
+    private LocalDate doj;
+
+    //M:1
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "department_id")
+    private Department department;
+
+    //M:1
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "manager_id")
+    private Employee manager;
+
+    //M:1
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "role_id")
+    private Role role;
+
+    @Column(name = "profile_pic_path")
+    private String profilePath;
+}
---
+++ backend/src/main/java/com/capestone/hrms_backend/entity/organization/Role.java
@@ -0,0 +1,18 @@
+package com.capestone.hrms_backend.entity.organization;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "roles")
+@Getter
+@Setter
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 20)
+    private String name;
+}
