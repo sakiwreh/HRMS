22089e7f89f0808a224e8d5305cc37cbccbd71f3
rehan-sakiwala07 <rehan.sakiwala@roimaint.com>
2026-02-09 12:48:47 +0530

    add global exception

---
+++ backend/src/main/java/com/capestone/hrms_backend/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.capestone.hrms_backend.exception;
+
+public class ResourceNotFoundException extends RuntimeException{
+    public ResourceNotFoundException(String message){
+        super(message);
+    }
+}
